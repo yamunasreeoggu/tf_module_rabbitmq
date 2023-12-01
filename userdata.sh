1a6fa0adeb1a43ff8cdf13be33883be77bbc1911
yamunasreeoggu <yamunasree321@gmail.com>
2023-12-01 17:07:59 -0500

    D75 | Yamuna | corretced userdata for rabbitmq

--- userdata.sh
+++ userdata.sh
@@ -2,4 +2,4 @@
 
 dnf install python3.11-pip ansible -y | tee -a /opt/userdata.log
 pip3.11 install botocore boto3 | tee -a /opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/yamunasreeoggu/infra_ansible main.yml -e role_name=${role_name} -e env=${env} | tee -a /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/yamunasreeoggu/roboshop_ansible roboshop.yml -e role_name=${role_name} -e env=${env} | tee -a /opt/userdata.log
